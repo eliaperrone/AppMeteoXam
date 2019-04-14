f8a0a83af99b9f44700a33db07c1dd4b0fc54d36
Marco Maccarini <marco.maccarini@student.supsi.ch>
2019-04-14 11:57:56 +0200

    Aggiunta richiesta meteo

--- MeteoApp/Models/Entry.cs
+++ MeteoApp/Models/Entry.cs
@@ -6,6 +6,8 @@ namespace MeteoApp
     {
         public int ID { get; set; }
         public string Name { get; set; }
+        public string State { get; set; }  //aggiunto dopo
+        public string Description { get; set; } //aggiunto dopo
         public double Lat { get; set; }
         public double Lon { get; set; }
         public double ActualTemperature { get; set; }
--- MeteoApp/Views/MeteoListPage.xaml.cs
+++ MeteoApp/Views/MeteoListPage.xaml.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
 using UIKit;
 using Xamarin.Forms;
+using Newtonsoft.Json.Linq;
 
 namespace MeteoApp
 {
@@ -23,7 +26,9 @@ namespace MeteoApp
             return  null;
         }
 
-        void OnItemAdded(object sender, EventArgs e)
+
+
+         void  OnItemAdded(object sender, EventArgs e)
         {
             // TODO modificare per compilare anche in ANDROID
 
@@ -35,19 +40,16 @@ namespace MeteoApp
             alert.AddButton("Cancel");
             alert.Message = "Please Enter a City";
             alert.AlertViewStyle = UIAlertViewStyle.PlainTextInput;
-            alert.Clicked += (object s, UIButtonEventArgs ev) =>
+            alert.Clicked += async (object s, UIButtonEventArgs ev) =>
             {
                 if (ev.ButtonIndex == 0)
                 {
                      input = alert.GetTextField(0).Text;
+                    Task < Entry > task = GetWeatherAsync(input);
 
-                    var appoggio = new Entry
-                    {
-                        ID = 2,
-                        Name = input
-                    };
-                    OnActionSheetSimpleClicked(sender, e);
-                    MeteoListViewModel.Entries.Add(appoggio);
+
+                    var appogio = await task;
+                   MeteoListViewModel.Entries.Add(appogio);
                 }
                 //faccio la richiesta ad openCage
                
@@ -55,6 +57,29 @@ namespace MeteoApp
             alert.Show();
 
         }
+
+        public async  Task<Entry>  GetWeatherAsync(string Nome)
+        {
+            var httpClient = new HttpClient();
+           Task<string> contentsTask = httpClient.GetStringAsync("https://api.openweathermap.org/data/2.5/weather?q=" + Nome + "&appid=c200173e4aeed3198803206f96382afe");
+      
+            var content = await contentsTask; 
+            var Appoggio = new Entry
+            {
+                Lat = (double)JObject.Parse(content)["coord"]["lat"],
+                Lon = (double)JObject.Parse(content)["coord"]["lon"],
+                Description  = (string)JObject.Parse(content)["weather"][0]["description"],
+                icon = "http://openweathermap.org/img/w/"+ (string)JObject.Parse(content)["weather"][0]["icon"]+ ".png" ,
+                ActualTemperature = (double)JObject.Parse(content)["main"]["temp"],
+                MinTemperature =  (double)JObject.Parse(content)["main"]["temp_min"],
+                MaxTemperature = (double)JObject.Parse(content)["main"]["temp_max"],
+                Name =(string)JObject.Parse(content)["name"],
+                State = (string)JObject.Parse(content)["sys"]["country"]
+            };
+            return Appoggio;
+        }
+
+
 
         void OnListItemSelected(object sender, SelectedItemChangedEventArgs e)
         {
@@ -66,6 +91,8 @@ namespace MeteoApp
                 });
             }
         }
+
+
 
         async void OnActionSheetSimpleClicked(object sender, EventArgs e)
         {
